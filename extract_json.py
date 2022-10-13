95e47a4529ee52e8acdf939f916eb726cc56cd32
shivikakbisen <48185934+sbisen@users.noreply.github.com>
2022-10-13 14:11:57 -0400

    Add files via upload

---
+++ extract_json.py
@@ -0,0 +1,16 @@
+import os, gzip, shutil
+
+dir_name = '/Users/shivikakbisen/Desktop/data'
+
+def gz_extract(directory):
+    extension = ".gz"
+    os.chdir(directory)
+    for item in os.listdir(directory): # loop through items in dir
+      if item.endswith(extension): # check for ".gz" extension
+          gz_name = os.path.abspath(item) # get full path of files
+          file_name = (os.path.basename(gz_name)).rsplit('.',1)[0] #get file name for file within
+          with gzip.open(gz_name,"rb") as f_in, open(file_name,"wb") as f_out:
+              shutil.copyfileobj(f_in, f_out)
+          os.remove(gz_name) # delete zipped file
+        
+gz_extract(dir_name)
